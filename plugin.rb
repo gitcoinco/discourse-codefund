352619e187166b7afcf31b1b92a7939ca79a5324
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2018-05-16 08:16:23 +0100

    setup plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: codefund-plugin
+# about: Ad Plugin for Codefund
+# version: 0.0.1
+# authors: Onuwa Nnachi Isaac (https://github.com/iamonuwa)
+# url: https://github.com/gitcoinco/discourse-codefund
